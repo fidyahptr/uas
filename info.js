583c6ebcea47fc78fc9c74a1d666cff7b36e929f
fidyahptr <62897343+fidyahptr@users.noreply.github.com>
2021-06-10 15:12:41 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,12 @@
+function AboutMe(){
+    const ab = document.getElementById("about");
+    ab.innerHTML = "Nama saya Fidyah Putri Rifayani <br> umur saya 18 tahun, tujuan saya membuat website ini adalah untuk menunjukan bahwa masih banyak wisata di Indonesia yang sangat indah dan jarang kita ketahui.";
+}
+
+function moreContak(){
+    const more = document.getElementById('demo').style.display='block';
+}
+
+function lessContact(){
+    const less = document.getElementById('demo').style.display='none';
+}
